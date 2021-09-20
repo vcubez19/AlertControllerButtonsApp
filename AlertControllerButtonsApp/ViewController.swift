269f01556b23bf7793308ed9a00939cb50a4ba6f
Vincent Cubit <vinniecubit@yahoo.com>
2021-09-19 19:37:59 -0500

    first

--- AlertControllerButtonsApp/ViewController.swift
+++ AlertControllerButtonsApp/ViewController.swift
@@ -5,15 +5,50 @@
 //  Created by Vincent Cubit on 9/19/21.
 //
 
+
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var labelResult: UILabel!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        labelResult.numberOfLines = 1
+        
+        
+    }
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+    
+        let alert = UIAlertController(title: "Warning", message: "Zombies are loose!", preferredStyle: .alert)
+    
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: { action -> Void in
+            self.labelResult.text = "OK"
+        })
+    
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: { action -> Void in
+            self.labelResult.text = "Cancel"
+        })
+    
+        let destroyAction = UIAlertAction(title: "Destroy", style: .destructive, handler: { action -> Void in
+            self.labelResult.text = "Destroy"
+        })
+        
+
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        alert.addAction(destroyAction)
+        self.present(alert, animated: true, completion: nil)
+        
     }
 
-
+    
 }
 
